8f8bd1c670d37a4aa55246231e6536ccacc590b9
Nitesh-Flutter-Dev <nitesh.warik@atriina.com>
2024-07-10 12:11:38 +0530

    "Nitesh - Assets path and constants added"

--- lib/aa_getx/core/constants/colors.dart
+++ lib/aa_getx/core/constants/colors.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
 
-Color colorBlue = Color.fromRGBO(48, 40, 131, 1);
-Color colorWhite = const Color(0xFFE0E0E0);
-Color colorSnackBarErrorBorder = const Color(0xFFEE5555);
+  const appTheme = Color(0xff1B2E6A);
+  const appTheme2 = Color(0xff020e34);
+  const red = Color(0xffEB5E50);
+  const colorWhite = Color(0xffFFFFFF);
+  const colorBg = Color(0xffF8F9FE);
+  const colorLightBlue = Color(0xffE2ECFF);
+  const colorDarkGray = Color(0xff333333);
+  const colorLightGray = Color(0xff595e68);
+  const colorLightGray2 = Color(0xffe7e9ee);
+  const colorGrey = Color(0xff949DB2);
+  const colorBlack = Color(0xff000000);
+  const colorTabBg = Color(0xffF8F9FE);
+  const colorLightAppTheme = Color(0xff12316F);
+  const colorForwardButton = Color(0xff868D9C);
+  const colorGreen = Color(0xff12B529);
+  const colorLightRed = Color(0xffFFF1EF);
+  const colorLightGreen = Color(0xffE2FFE6);
+  const colorLightYellow = Color(0xffFFF8E8);
+  const colorDarkYellow = Color(0xffffe36a);
+  const colorLightSky = Color(0xffE7EBFB);
+  const colorRed = Color(0xffEECAC5);
+  const colorDisable = Color(0xffdee0e0);
+  Color colorSnackBarErrorBorder = const Color(0xFFEE5555);
+  Color colorBlue = Color.fromRGBO(48, 40, 131, 1);
+
+
+
