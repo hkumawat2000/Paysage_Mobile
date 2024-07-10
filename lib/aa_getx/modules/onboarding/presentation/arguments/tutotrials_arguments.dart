9dca97ea11fdb4c056a1ba76c1760e6025b53350
Nitesh-Flutter-Dev <nitesh.warik@atriina.com>
2024-07-10 12:16:51 +0530

    "Nitesh - tutorials/ walkthrough arguments added"

---
+++ lib/aa_getx/modules/onboarding/presentation/arguments/tutotrials_arguments.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:lms/aa_getx/modules/onboarding/domain/entity/onboarding_response_entity.dart';
+
+class TutotrialsArguments {
+  List<OnBoardingDataEntity> onboardingDataEntity;
+  
+  TutotrialsArguments({
+    required this.onboardingDataEntity,
+  });
+}
