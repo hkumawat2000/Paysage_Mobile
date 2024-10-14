14017ebd0b6c14f0c0fd61b6a67144f22b0a9e9d
Himanshu Kumawat <himanshu@Himanshus-MacBook-Pro.local>
2024-10-14 21:18:51 +0530

    CR:Himanshu api integaration done

--- lib/aa_getx/modules/feedback/data/data_source/feedback_data_source.dart
+++ lib/aa_getx/modules/feedback/data/data_source/feedback_data_source.dart
@@ -1,3 +1,7 @@
+import 'package:dio/dio.dart';
+import 'package:lms/aa_getx/core/error/dio_error_handler.dart';
+import 'package:lms/aa_getx/core/error/exception.dart';
+import 'package:lms/aa_getx/core/network/apis.dart';
 import 'package:lms/aa_getx/modules/feedback/data/models/request/feedback_request_model.dart';
 import 'package:lms/aa_getx/modules/feedback/data/models/response/feedback_response_model.dart';
 
@@ -9,6 +13,20 @@ abstract class FeedbackDataSource {
 
 }
 
-class FeedbackDataSourceImpl with BaseDio implements CibilDataSource {
+class FeedbackDataSourceImpl with BaseDio implements FeedbackDataSource {
+  @override
+  Future<FeedbackResponseModel> submitFeedback(FeedbackRequestModel feedbackRequestModel) async {
+    Dio dio = await getBaseDio();
+    try {
+      final response = await dio.post(Apis.feedback, data: feedbackRequestModel.toJson());
+      if (response.statusCode == 200) {
+        return FeedbackResponseModel.fromJson(response.data);
+      } else {
+        throw ServerException(response.statusMessage);
+      }
+    } on DioException catch (e) {
+      throw handleDioClientError(e);
+    }
+  }
 
 }
