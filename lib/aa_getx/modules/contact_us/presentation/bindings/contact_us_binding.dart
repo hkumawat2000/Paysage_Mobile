436bb15681e0213eb6c10a1331027e3542c82d69
Himanshu Kumawat <himanshu@Himanshus-MacBook-Pro.local>
2024-10-14 18:38:30 +0530

    CR:Himanshu contact us api integration done

--- lib/aa_getx/modules/contact_us/presentation/bindings/contact_us_binding.dart
+++ lib/aa_getx/modules/contact_us/presentation/bindings/contact_us_binding.dart
@@ -1,10 +1,30 @@
 import 'package:get/get.dart';
+import 'package:lms/aa_getx/modules/contact_us/data/data_source/contact_us_data_source.dart';
+import 'package:lms/aa_getx/modules/contact_us/data/repositories/contact_us_respository_impl.dart';
+import 'package:lms/aa_getx/modules/contact_us/domain/usecases/contact_us_usecase.dart';
 import 'package:lms/aa_getx/modules/contact_us/presentation/controllers/contact_us_controller.dart';
 
 class ContactUsBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<ContactUsController>(() => ContactUsController());
+
+
+    Get.lazyPut<ContactUsDataSourceImpl>(
+          () => ContactUsDataSourceImpl(),
+    );
+
+    Get.lazyPut<ContactUsRepositoryImpl>(
+          () => ContactUsRepositoryImpl(
+        Get.find<ContactUsDataSourceImpl>(),
+      ),
+    );
+
+    Get.lazyPut<ContactUsUsecase>(
+            () => ContactUsUsecase(Get.find<ContactUsRepositoryImpl>()));
+
+    Get.lazyPut<ContactUsController>(() => ContactUsController(
+      Get.find<ContactUsUsecase>()
+    ));
 
   }
 
--- lib/aa_getx/modules/contact_us/presentation/controllers/contact_us_controller.dart
+++ lib/aa_getx/modules/contact_us/presentation/controllers/contact_us_controller.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lms/aa_getx/core/utils/common_widgets.dart';
+import 'package:lms/aa_getx/core/utils/data_state.dart';
+import 'package:lms/aa_getx/core/utils/utility.dart';
+import 'package:lms/aa_getx/modules/contact_us/domain/entity/request/contactus_request_entity.dart';
+import 'package:lms/aa_getx/modules/contact_us/domain/entity/response/contactus_response_entity.dart';
+import 'package:lms/aa_getx/modules/contact_us/domain/usecases/contact_us_usecase.dart';
+
+import '../../../../core/constants/strings.dart';
 
 class ContactUsController extends GetxController {
 
@@ -8,8 +16,22 @@ class ContactUsController extends GetxController {
 
   RxString contactUsMessageTxt = "".obs;
 
-  contactUsAPI(){
+  final ContactUsUsecase contactUsUsecase;
 
+  ContactUsController(this.contactUsUsecase);
 
+  contactUsAPI() async {
+    showDialogLoading(Strings.please_wait);
+    DataState<ContactUsResponseEntity> response = await contactUsUsecase.call(ContactUsParams(
+        contactUsRequestEntity: ContactUsRequestEntity(
+          message: contactUsMessageTxt.value
+        )
+    ));
+    Get.back();
+    if (response is DataSuccess) {
+
+    } else if (response is DataFailed) {
+      Utility.showToastMessage(response.error!.message);
+    }
   }
 }
