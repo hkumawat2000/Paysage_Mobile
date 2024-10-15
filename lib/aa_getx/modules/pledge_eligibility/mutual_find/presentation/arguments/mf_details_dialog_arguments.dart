d1050f43e28a04e48b9e58b171bfa0a05dbaf3be
Nitesh-Flutter-Dev <nitesh.warik@atriina.com>
2024-10-15 13:51:23 +0530

    CR- Nitesh - MfDetailsViewDialog - WIP

--- lib/aa_getx/modules/pledge_eligibility/mutual_find/presentation/arguments/mf_details_dialog_arguments.dart
+++ lib/aa_getx/modules/pledge_eligibility/mutual_find/presentation/arguments/mf_details_dialog_arguments.dart
@@ -1,7 +1,7 @@
 import 'package:lms/aa_getx/modules/pledge_eligibility/mutual_find/domain/entities/response/isin_details_response_entity.dart';
 import 'package:lms/aa_getx/modules/pledge_eligibility/mutual_find/domain/entities/response/mf_scheme_response_entity.dart';
 
-class MfDetailsDialogArguments{
+class MfDetailsDialogArguments {
   List<SchemesListEntity> selectedSchemeList = [];
   SchemesListEntity scheme;
   List<IsinDetailsResponseEntity>? isinDetails;
@@ -10,6 +10,13 @@ class MfDetailsDialogArguments{
   String? selectedUnit;
   List<SchemesListEntity> schemeListItems;
 
-  MfDetailsDialogArguments({});
-
+  MfDetailsDialogArguments({
+    required this.selectedSchemeList,
+    required this.scheme,
+    required this.isinDetails,
+    required this.schemeType,
+    required this.lender,
+    required this.selectedUnit,
+    required this.schemeListItems,
+  });
 }
