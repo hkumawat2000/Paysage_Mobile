53e5712af5b37ab1b28061685e63382ff977ac50
Himanshu Kumawat <himanshu@Himanshus-MacBook-Pro.local>
2024-10-25 17:42:05 +0530

    CR:Himanshu integrated loan closer api

--- lib/aa_getx/modules/pledged_securities/data/data_sources/pledged_securities_api.dart
+++ lib/aa_getx/modules/pledged_securities/data/data_sources/pledged_securities_api.dart
@@ -4,11 +4,15 @@ import 'package:lms/aa_getx/core/error/dio_error_handler.dart';
 import 'package:lms/aa_getx/core/error/exception.dart';
 import 'package:lms/aa_getx/core/network/apis.dart';
 import 'package:lms/aa_getx/core/network/base_dio.dart';
+import 'package:lms/aa_getx/modules/pledged_securities/data/models/request/loan_closer_request_model.dart';
+import 'package:lms/aa_getx/modules/pledged_securities/data/models/response/loan_closer_response_model.dart';
 import 'package:lms/aa_getx/modules/pledged_securities/data/models/response/my_pledged_securities_details_response_model.dart';
 import 'package:lms/aa_getx/modules/pledged_securities/data/models/request/my_pledged_securities_request_model.dart';
 
 abstract class PledgedSecuritiesApi {
   Future<MyPledgedSecuritiesDetailsResponseModel> getMyPledgedSecurities(MyPledgedSecuritiesRequestModel myPledgedSecuritiesRequestModel);
+
+  Future<LoanCloserResponseModel> loanCloser(LoanCloserRequestModel loanCloserRequestModel);
 }
 
 class PledgedSecuritiesApiImpl extends PledgedSecuritiesApi with BaseDio{
@@ -20,6 +24,22 @@ class PledgedSecuritiesApiImpl extends PledgedSecuritiesApi with BaseDio{
           queryParameters: myPledgedSecuritiesRequestModel.toJson());
       if (response.statusCode == 200) {
         return MyPledgedSecuritiesDetailsResponseModel.fromJson(response.data);
+      } else {
+        throw ServerException(response.statusMessage);
+      }
+    } on DioException catch (e) {
+      throw handleDioClientError(e);
+    }
+  }
+
+  @override
+  Future<LoanCloserResponseModel> loanCloser(LoanCloserRequestModel loanCloserRequestModel) async {
+    Dio dio = await getBaseDio();
+    try {
+      final response = await dio.get(Apis.loanCloser,
+          queryParameters: loanCloserRequestModel.toJson());
+      if (response.statusCode == 200) {
+        return LoanCloserResponseModel.fromJson(response.data);
       } else {
         throw ServerException(response.statusMessage);
       }
